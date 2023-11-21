083a58dd74769cba9a3d2de25c6b3568861c0e23
Taha Adeel Mohammed <cs20btech11052@iith.ac.in>
2023-11-22 05:03:22 +0530

    Removed old code

--- Code/STR-model/data_processing/create_resampled_data.py
+++
@@ -1,6 +0,0 @@
-# Load all data from jsons
-# Create a dataloader with resampler
-# save out with                 output.append({"stroke": p,
-#                                "text":GT_DATA[name],
-#                                "id": name,
-#                                "distance": np.average(distance)
--- Code/STR-model/data_processing/online_coordinate_data/create_synthetic_dataset.py
+++
@@ -1,4 +0,0 @@
-
-
-
-"/media/data/GitHub/simple_hwr/data/synthetic_online/train_synth_sample.json"
