b0ca49b4526eed0c288954bf9d7a7cef4e66f8ac
Taha Adeel Mohammed <cs20btech11052@iith.ac.in>
2023-11-23 21:03:05 +0530

    Debugged dataloader

--- Code/STR_model/data_processing/gt_resampling.py
+++ Code/STR_model/data_processing/gt_resampling.py
@@ -5,8 +5,9 @@ import sys
 sys.path.append('../')
 from util.get_word_strokes import get_strokes
 
-def interpolate_stroke(points, m, num_EoS_extra=5):
+def interpolate_stroke(points: np.ndarray, m: int, num_EoS_extra: int=5) -> list[np.ndarray]:
     ''' Interpolate points along a single stroke.
+    
     Parameters:
     - points: n x (x, y, SoS, EoS)
     - m: number of equidistant points to output
@@ -19,7 +20,10 @@ def interpolate_stroke(points, m, num_EoS_extra=5):
     cumulative_distance = np.insert(cumulative_distance, 0, 0)  # Insert 0 at the beginning
     
     # Normalize the cumulative distance to range [0, 1]
-    normalized_distance = cumulative_distance / cumulative_distance[-1]
+    if cumulative_distance[-1] == 0:
+        normalized_distance = np.zeros_like(cumulative_distance)
+    else:
+        normalized_distance = cumulative_distance / cumulative_distance[-1]
     
     # Interpolate equidistant points
     interpolated_normalized_distance = np.linspace(0, 1, max(1, m-num_EoS_extra))
@@ -30,22 +34,25 @@ def interpolate_stroke(points, m, num_EoS_extra=5):
     # Add the last point num_EoS_extra times
     interpolated_points[max(1, m-num_EoS_extra):] = points[-1]
     
-    # Set the SoS and EoS flags
+    # Set the SoS flag
     interpolated_points[0, 2] = 1
     
     return interpolated_points.tolist()
 
 
-def interpolate_word_strokes(word_strokes, m):
-    ''' Interpolate points along a word with multiple strokes.
+def resample_strokes(word_strokes: np.ndarray, m: int,  num_EoS_extra: int=5) -> np.ndarray:
+    ''' Interpolate points along a word with multiple strokes to get m equidistant points describing the same strokes.
+    
     Parameters:
     - word_strokes: list of n x (x, y, SoS, EoS)
     - m: number of equidistant points to output
+    - num_EoS_extra: number of extra EoS points to add to help with class imbalance during training
+    
     Returns: m x (x, y, SoS, EoS)
     '''
     interpolated_word_strokes = []
     strokes = get_strokes(word_strokes)
-    n = len(word_strokes)
+    n = word_strokes.shape[0]
     m_cur = 0
     for i, stroke in enumerate(strokes):
         if i < len(strokes)-1: 
@@ -53,23 +60,27 @@ def interpolate_word_strokes(word_strokes, m):
             m_cur += stroke_len
         else:
             stroke_len = m - m_cur
-        interpolated_word_strokes += interpolate_stroke(stroke, stroke_len)
+        if stroke_len == 0: continue
+        interpolated_word_strokes += interpolate_stroke(stroke, stroke_len, num_EoS_extra)
     assert len(interpolated_word_strokes) == m, 'Number of interpolated points is not equal to m'
         
     return np.array(interpolated_word_strokes)
     
-def draw_interpolated_strokes(strokes, interpolated_strokes):
+def draw_interpolated_strokes(strokes: np.ndarray, interpolated_strokes: np.ndarray) -> None:
     ''' Draw the original and interpolated strokes.'''
     # Plot the original points and the interpolated stroke
     plt.scatter(strokes[:,0], strokes[:,1])
     print(f'Num of strokes: {len(get_strokes(strokes))}')
     for interpolate_stroke in get_strokes(interpolated_strokes):
         plt.plot(interpolate_stroke[:,0], interpolate_stroke[:,1], color='red')
+        # plt.scatter(interpolate_stroke[:,0], interpolate_stroke[:,1], color='red')
+    plt.legend(['Original points', 'Interpolated stroke'])
     plt.show()
     
 # %%
-def test_gt_resampling():
-    file_path = 'stroke_43757.npy'
+def test_gt_resampling() -> None:
+    ''' Test the ground truth resampling.'''
+    file_path = '../../../DataSet/IAM-Online/Resized_Dataset/Train/Strokes/stroke_2.npy'
     points = np.load(file_path)
     points = np.delete(points, 2, axis=1) # Remove the 3rd dimension (time) from the points (Keep 0, 1, 3, 4)
 
@@ -77,13 +88,14 @@ def test_gt_resampling():
 
     # Set the number of equidistant points for interpolation
     img_width = max(points[:, 0])
-    num_equidistant_points = int(img_width) * 4
+    num_equidistant_points = int(img_width)
     print(f'{int(img_width)=}, {num_equidistant_points=}')
 
     # Get the interpolated points
-    interpolated_points = interpolate_word_strokes(points, num_equidistant_points)
+    interpolated_points = resample_strokes(points, num_equidistant_points)
 
     draw_interpolated_strokes(points, interpolated_points)
     
 if __name__ == '__main__':
     test_gt_resampling()
+# %%
--- Code/STR_model/util/get_word_strokes.py
+++ Code/STR_model/util/get_word_strokes.py
@@ -4,12 +4,12 @@ def get_strokes(word_strokes: np.ndarray) -> list[np.ndarray]:
     ''' Get strokes from a word with multiple strokes.'''
     word_strokes[-1, 3] = 1  # Set the EoS flag of the last point to 1
     strokes = []
-    SoS_indices = np.where(word_strokes[:, 2] == 1)[0]
-    EoS_indices = np.zeros(len(SoS_indices), dtype=int)
-    for i in range(len(SoS_indices)):
-        # Find the first EoS after the current SoS (There can be multiple EoS per SoS)
-        EoS_indices[i] = np.where(word_strokes[SoS_indices[i]:, 3] == 1)[0][0] + SoS_indices[i]
-    
-    for i, j in zip(SoS_indices, EoS_indices):
-        strokes.append(word_strokes[i:j+1])
+    start = -1
+    for cur in range(word_strokes.shape[0]):
+        if word_strokes[cur, 2] == 1:
+            start = cur
+        if word_strokes[cur, 3] == 1 and start != -1:
+            strokes.append(word_strokes[start:cur+1])
+            start = -1
+            
     return strokes
